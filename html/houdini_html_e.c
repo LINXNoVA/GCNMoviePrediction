1ff64238c9230a2c709ab8ee1beccdc980b2facf
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2020-04-11 16:46:27 +0000

    fix compiling

--- html/houdini_html_e.c
+++ html/houdini_html_e.c
@@ -4,4 +4,9 @@
 
 #include "houdini.h"
 
-#define ESCAPE_GROW_FACTOR(x) (((x) * 12) /
+#define ESCAPE_GROW_FACTOR(x) (((x) * 12) / 10) /* this is very scientific, yes */
+
+/**
+ * According to the OWASP rules:
+ *
+ * 
