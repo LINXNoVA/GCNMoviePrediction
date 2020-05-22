054ab8591806d55dfedfa08de46c6a5219f1c56f
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2020-05-22 21:22:25 +0000

    save changes

--- html/html.h
+++ html/html.h
@@ -3,4 +3,6 @@
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission 
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHO
