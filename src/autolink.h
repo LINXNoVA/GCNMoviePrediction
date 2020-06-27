6b1381e7edb148fffb3cb0cf6e329116446de2ec
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2020-06-27 19:22:57 +0000

    update bundle.js

--- src/autolink.h
+++ src/autolink.h
@@ -1,4 +1,5 @@
 /*
  * Copyright (c) 2011, Vicent Marti
  *
- * Permission to use, copy, modify, an
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby 
