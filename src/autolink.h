5e052942b3bbe2c3c514c20020a382bc09bf58d3
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2020-07-16 18:04:51 +0000

    v2.0.5

--- src/autolink.h
+++ src/autolink.h
@@ -32,4 +32,7 @@ sd_autolink_issafe(const uint8_t *link, size_t link_len);
 
 size_t
 sd_autolink__www(size_t *rewind_p, struct buf *link,
-	uint8_t *data, size_t offset, size_t s
+	uint8_t *data, size_t offset, size_t size, unsigned int flags);
+
+size_t
+sd_autolink__email(size_t *rewind_p, s
