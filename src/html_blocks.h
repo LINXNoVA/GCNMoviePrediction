481638750e80b840e2ee7b90f599d5f4a828c29e
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2020-07-23 20:05:23 +0000

    Fix some failing tests

--- src/html_blocks.h
+++ src/html_blocks.h
@@ -1,2 +1,3 @@
 /* C code produced by gperf version 3.0.3 */
-/* Command-line: gperf 
+/* Command-line: gperf -N find_block_tag -H hash_block_tag -C -c -E --ignore-case html_block_names.txt  */
+/* Computed positi
