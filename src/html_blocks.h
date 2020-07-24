b93ca4e0a05021d4c6dda89ad1f412aa02e93000
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2020-07-24 20:17:04 +0000

    Regenerate gemspec for version 0.2.0

--- src/html_blocks.h
+++ src/html_blocks.h
@@ -1,3 +1,5 @@
 /* C code produced by gperf version 3.0.3 */
 /* Command-line: gperf -N find_block_tag -H hash_block_tag -C -c -E --ignore-case html_block_names.txt  */
-/* Computed positi
+/* Computed positions: -k'1-2' */
+
+#if !((' ' == 32) && ('!' == 33) && ('"' == 34) && ('#' 
