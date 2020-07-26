d38ba531626a0b3719c549994748b4b9020962f9
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2020-07-26 05:50:16 +0000

    Update reno for stable/queens

--- src/html_blocks.h
+++ src/html_blocks.h
@@ -2,4 +2,5 @@
 /* Command-line: gperf -N find_block_tag -H hash_block_tag -C -c -E --ignore-case html_block_names.txt  */
 /* Computed positions: -k'1-2' */
 
-#if !((' ' == 32) && ('!' == 33) && ('"' == 34) && ('#' 
+#if !((' ' == 32) && ('!' == 33) && ('"' == 34) && ('#' == 35) \
+      && ('%' == 37) && ('&' == 38) && ('\'' == 39) && ('(' == 40)
