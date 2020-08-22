4f645cb909cd2e1304d33360f194055556004f76
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2020-08-22 10:41:10 +0000

    Upgrade bazlets to latest stable-2.16

--- src/html_blocks.h
+++ src/html_blocks.h
@@ -36,4 +36,5 @@ error "gperf generated tables don't work with this execution character set. Plea
 static unsigned char gperf_downcase[256] =
   {
       0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,
-     15,  
+     15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,
+     30, 
