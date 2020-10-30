77aa3e8add0ac2ce823be15929bc6a3136a170a5
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2020-10-30 13:10:42 +0000

    sparc64: Fix off by one in trampoline TLB mapping installation loop.

--- src/markdown.c
+++ src/markdown.c
@@ -4,4 +4,5 @@
  * Copyright (c) 2009, Natacha Porté
  * Copyright (c) 2011, Vicent Marti
  *
- * Permission to use, copy, modify, and d
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, 
