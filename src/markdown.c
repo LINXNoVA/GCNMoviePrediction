896b8a67be265579e4ffdee11b005327914091f6
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2020-12-09 06:39:49 +0000

    schedule update

--- src/markdown.c
+++ src/markdown.c
@@ -69,4 +69,5 @@ static size_t char_codespan(struct buf *ob, struct sd_markdown *rndr, uint8_t *d
 static size_t char_escape(struct buf *ob, struct sd_markdown *rndr, uint8_t *data, size_t offset, size_t size);
 static size_t char_entity(struct buf *ob, struct sd_markdown *rndr, uint8_t *data, size_t offset, size_t size);
 static size_t char_langle_tag(struct buf *ob, struct sd_markdown *rndr, uint8_t *data, size_t offset, size_t size);
-static size_t char_autolink_url(struct buf *ob, struct sd_markdown *rndr, uint8_t *data, size_t offset,
+static size_t char_autolink_url(struct buf *ob, struct sd_markdown *rndr, uint8_t *data, size_t offset, size_t size);
+static size_t char_autolink_email(struct buf *ob, s
