8a437e6747889262a3881cffe5a8b4c18f508b43
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2021-01-04 17:50:48 +0000

    Add ignore files

--- src/markdown.c
+++ src/markdown.c
@@ -174,4 +174,7 @@ hash_link_ref(const uint8_t *link_ref, size_t length)
 	unsigned int hash = 0;
 
 	for (i = 0; i < length; ++i)
-		hash 
+		hash = tolower(link_ref[i]) + (hash << 6) + (hash << 16) - hash;
+
+	return hash;
+}
