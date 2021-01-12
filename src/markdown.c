2d09ecb6a4488d117a28759f4c7d0527f5427613
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2021-01-12 11:38:49 +0000

    Add missing test file.

--- src/markdown.c
+++ src/markdown.c
@@ -197,4 +197,7 @@ add_link_ref(
 }
 
 static struct link_ref *
-find_link_ref(struct link_ref **references, uin
+find_link_ref(struct link_ref **references, uint8_t *name, size_t length)
+{
+	unsigned int hash = hash_link_ref(name, length);
+	struct link_ref
