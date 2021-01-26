4095da133c208d914c75ecd9525d3b096fb26168
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2021-01-26 16:27:06 +0000

    travis?

--- src/markdown.c
+++ src/markdown.c
@@ -296,4 +296,8 @@ tag_length(uint8_t *data, size_t size, enum mkd_autolink *autolink)
 	if (size < 3) return 0;
 
 	/* begins with a '<' optionally followed by '/', followed by letter or number */
-	if (dat
+	if (data[0] != '<') return 0;
+	i = (data[1] == '/') ? 2 : 1;
+
+	if (!isalnum(data[i]))
+	
