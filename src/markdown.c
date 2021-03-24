d17d298e24385c98c7607b75c86b0b84a4997136
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2021-03-24 22:26:02 +0000

    USB: fix build error when CONFIG_PM_SLEEP isn't enabled

--- src/markdown.c
+++ src/markdown.c
@@ -500,4 +500,7 @@ parse_emph1(struct buf *ob, struct sd_markdown *rndr, uint8_t *data, size_t size
 
 		if (data[i] == c && !_isspace(data[i - 1])) {
 
-			if (rndr->ext_flags & MKDEXT_NO_INTRA_EMPH
+			if (rndr->ext_flags & MKDEXT_NO_INTRA_EMPHASIS) {
+				if (i + 1 < size && isalnum(data[i + 1]))
+					continue;
+			
