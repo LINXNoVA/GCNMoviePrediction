b990939dd665af7b71d28931a9d82f76bf90346a
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2021-04-03 15:52:12 +0000

    crypto: caam - fix RNG buffer cache alignment

--- src/markdown.c
+++ src/markdown.c
@@ -565,4 +565,5 @@ parse_emph3(struct buf *ob, struct sd_markdown *rndr, uint8_t *data, size_t size
 			continue;
 
 		if (i + 2 < size && data[i + 1] == c && data[i + 2] == c && rndr->cb.triple_emphasis) {
-			/* triple
+			/* triple symbol found */
+			struct buf *work = rndr_newbuf(rndr, BU
