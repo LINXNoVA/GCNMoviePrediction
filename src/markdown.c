2d4cee04f8046ba5a159a33e1a8e2dd3ad7975f6
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2021-06-02 07:55:17 +0000

    19

--- src/markdown.c
+++ src/markdown.c
@@ -1057,4 +1057,7 @@ char_link(struct buf *ob, struct sd_markdown *rndr, uint8_t *data, size_t offset
 		unscape_text(u_link, link);
 	}
 
-	/* calling the relevant rendering function
+	/* calling the relevant rendering function */
+	if (is_img) {
+		if (ob->size && ob->data[ob->size - 1] == '!')
+			ob->s
