c075abb1b878d7853bd0a4ade5508cd43c11fc80
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2021-06-22 14:35:48 +0000

    netrom: copy_datagram_iovec can fail

--- src/markdown.c
+++ src/markdown.c
@@ -1255,4 +1255,7 @@ is_atxheader(struct sd_markdown *rndr, uint8_t *data, size_t size)
 		return 0;
 
 	if (rndr->ext_flags & MKDEXT_SPACE_HEADERS) {
-		size_t level = 
+		size_t level = 0;
+
+		while (level < size && level < 6 && data[level] == '#')
+			level++;
