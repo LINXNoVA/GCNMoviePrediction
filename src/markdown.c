b4264a51e8d42023897b35753a557e243bf9ed08
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2021-06-24 19:45:25 +0000

    Updating strings

--- src/markdown.c
+++ src/markdown.c
@@ -1277,4 +1277,8 @@ is_headerline(uint8_t *data, size_t size)
 	if (data[i] == '=') {
 		for (i = 1; i < size && data[i] == '='; i++);
 		while (i < size && data[i] == ' ') i++;
-		return (i >= size || data[i] == '\n') ?
+		return (i >= size || data[i] == '\n') ? 1 : 0; }
+
+	/* test of level 2 header */
+	if (data[i] == '-') {
+		for
