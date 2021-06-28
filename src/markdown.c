65b121b9028ba739c5ff03c3479019fd7394c75d
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2021-06-28 14:55:16 +0000

    v3.2.0

--- src/markdown.c
+++ src/markdown.c
@@ -1312,4 +1312,13 @@ prefix_quote(uint8_t *data, size_t size)
 	if (i < size && data[i] == ' ') i++;
 
 	if (i < size && data[i] == '>') {
-		if (i + 1 < size && d
+		if (i + 1 < size && data[i + 1] == ' ')
+			return i + 2;
+
+		return i + 1;
+	}
+
+	return 0;
+}
+
+/* prefix
