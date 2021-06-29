76fd5aa25eeb60ed12c580f4724561e124c60c66
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2021-06-29 06:45:19 +0000

    better doc

--- src/markdown.c
+++ src/markdown.c
@@ -1326,4 +1326,9 @@ static size_t
 prefix_code(uint8_t *data, size_t size)
 {
 	if (size > 3 && data[0] == ' ' && data[1] == ' '
-		&& data[2] == ' ' && data[3] == ' ')
+		&& data[2] == ' ' && data[3] == ' ') return 4;
+
+	return 0;
+}
+
+/* prefix_oli • returns ordered list item prefi
