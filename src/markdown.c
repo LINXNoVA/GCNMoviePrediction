47510ae25c6f4701932a1766e95ef48ae64e27c9
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2022-01-22 16:21:48 +0000

    Create task1.c

--- src/markdown.c
+++ src/markdown.c
@@ -1911,4 +1911,7 @@ parse_htmlblock(struct buf *ob, struct sd_markdown *rndr, uint8_t *data, size_t 
 		i++;
 
 	if (i < size)
-		curtag = find_block_ta
+		curtag = find_block_tag((char *)data + 1, (int)i - 1);
+
+	/* handling of special cases */
+	if
