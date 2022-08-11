cd62969d044efd5377ec3f34c08a9c412510e462
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2022-08-11 09:33:27 +0000

    Remove DOS EOL char

--- src/markdown.c
+++ src/markdown.c
@@ -2438,4 +2438,7 @@ sd_markdown_new(
 	if (extensions & MKDEXT_AUTOLINK) {
 		md->active_char[':'] = MD_CHAR_AUTOLINK_URL;
 		md->active_char['@'] = MD_CHAR_AUTOLINK_EMAIL;
-		md->active_char['w'] 
+		md->active_char['w'] = MD_CHAR_AUTOLINK_WWW;
+	}
+
+	if (extensions & MKDEXT_SUPERSCRI
