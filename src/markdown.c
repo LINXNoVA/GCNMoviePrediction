03ec080e72dec81e94549b53432f3f4e09defbed
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2022-10-02 12:57:00 +0000

    Remove .DS_Store file

--- src/markdown.c
+++ src/markdown.c
@@ -2537,4 +2537,7 @@ sd_markdown_free(struct sd_markdown *md)
 		bufrelease(md->work_bufs[BUFFER_SPAN].item[i]);
 
 	for (i = 0; i < (size_t)md->work_bufs[BUFFER_BLOCK].asize; ++i)
-		bufrelease(md->work_bufs[BUFFER_BLOCK].item[
+		bufrelease(md->work_bufs[BUFFER_BLOCK].item[i]);
+
+	stack_free(&md->work_bufs[BUFFER_SPAN]);
+	stack_free(&md->w
