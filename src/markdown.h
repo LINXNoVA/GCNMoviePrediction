d67401e110037a21ec14ad678b941867a5fa8e96
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2022-10-16 16:18:59 +0000

    microfix

--- src/markdown.h
+++ src/markdown.h
@@ -4,4 +4,5 @@
  * Copyright (c) 2009, Natacha Porté
  *
  * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby gran
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission
