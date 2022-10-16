84d1ec260bc4321b1cfb723b5b720a0bf7a8203f
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2022-10-16 07:21:33 +0000

    Tweak comment.

--- src/markdown.h
+++ src/markdown.h
@@ -3,4 +3,5 @@
 /*
  * Copyright (c) 2009, Natacha Porté
  *
- * Permission to use, copy, modify, and distrib
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby gran
