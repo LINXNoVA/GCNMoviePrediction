63356f38cea347297c85758dc88a5a6c96764101
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2022-10-23 05:48:41 +0000

    fix words

--- src/markdown.h
+++ src/markdown.h
@@ -5,4 +5,6 @@
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AU
