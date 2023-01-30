734711837e068dc9fc22f27c76c4987623290355
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2023-01-30 17:51:42 +0000

    loplugin:stringconstant: elide explicit ctor usage (automatic rewrite)

--- src/markdown.h
+++ src/markdown.h
@@ -84,4 +84,4 @@ struct sd_callbacks {
 	int (*emphasis)(struct buf *ob, const struct buf *text, void *opaque);
 	int (*image)(struct buf *ob, const struct buf *link, const struct buf *title, const struct buf *alt, void *opaque);
 	int (*linebreak)(struct buf *ob, void *opaque);
-	int (*link)(struct buf *ob, const 
+	int (*link)(struct buf *ob, const struct buf *link, const struct buf *title, const struct buf *content, void *
