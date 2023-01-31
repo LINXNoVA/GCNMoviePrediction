e2b0cb6e7e7132ed41fabac569e08a1db84e1026
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2023-01-31 10:44:34 +0000

    Run docker/update.sh

--- src/markdown.h
+++ src/markdown.h
@@ -84,4 +84,5 @@ struct sd_callbacks {
 	int (*emphasis)(struct buf *ob, const struct buf *text, void *opaque);
 	int (*image)(struct buf *ob, const struct buf *link, const struct buf *title, const struct buf *alt, void *opaque);
 	int (*linebreak)(struct buf *ob, void *opaque);
-	int (*link)(struct buf *ob, const struct buf *link, const struct buf *title, const struct buf *content, void *
+	int (*link)(struct buf *ob, const struct buf *link, const struct buf *title, const struct buf *content, void *opaque);
+	int (*raw_html_tag)(struct buf *ob, const struct buf
