f99187601b6ea012f8f6f33a54b036bc2044af63
LINXNoVA <97325946+LINXNoVA@users.noreply.github.com>
2023-02-24 13:04:50 +0000

    added CHANGELOG.md

--- src/stack.h
+++ src/stack.h
@@ -10,3 +10,21 @@ extern "C" {
 
 struct stack {
 	void **item;
+	size_t size;
+	size_t asize;
+};
+
+void stack_free(struct stack *);
+int stack_grow(struct stack *, size_t);
+int stack_init(struct stack *, size_t);
+
+int stack_push(struct stack *, void *);
+
+void *stack_pop(struct stack *);
+void *stack_top(struct stack *);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
